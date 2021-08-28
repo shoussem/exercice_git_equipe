008d992af22341506b211ba07a7374536d79b1fe
shoussem <houssemtun@yahoo.fr>
2021-08-28 00:07:15 -0400

    “conflit”

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //houssem saaidi
 console.log("test")
 //test
-=======
+//=======
 // bla bla
 1=1;
->>>>>>> b53079c358e533a6bc94239594d11d106e12dcc5
+//>>>>>>> b53079c358e533a6bc94239594d11d106e12dcc5
 
